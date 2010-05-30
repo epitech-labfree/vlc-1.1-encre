baaca1410f95299ae44b0840db86fb3ea56ab620
Rémi Denis-Courmont <remi@remlab.net>
2010-05-30 11:28:26 +0300

    Prevent loading KDE plugins for Qt4
    Since they cause crashes pretty much all the time (KDE bug #234484).
    
    Fixes: LP#408719, Debian #580396.

--- bin/override.c
+++ bin/override.c
@@ -280,6 +280,17 @@ int (*XSetIOErrorHandler (int (*handler) (Display *))) (Display *)
     return CALL(XSetIOErrorHandler, handler);
 }
 #endif
+
+/*** Buggy shared objects ***/
+#include <string.h>
+
+void *dlopen (const char *path, int flag)
+{
+    if (strstr (path, "/gui_platform/libkde.so"))
+        return NULL; /* Oh no, not that one! */
+    return CALL(dlopen, path, flag);
+}
+
 #else
 void vlc_enable_override (void)
 {
