646aab6e5d3e5f24070e3b630efa460d686a850a
Jean-Baptiste Kempf <jb@videolan.org>
2010-06-12 02:28:34 +0200

    Qt: videoWidget shouldn't be show/hide(), the parent stackWidget should toggle. (cherry picked from commit 8a0bd27b9524be4ba2b8b67e030ae9d9ca096b03)
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/gui/qt4/components/interface_widgets.cpp
+++ modules/gui/qt4/components/interface_widgets.cpp
@@ -74,6 +74,7 @@ VideoWidget::VideoWidget( intf_thread_t *_p_i )
     layout->setContentsMargins( 0, 0, 0, 0 );
     setLayout( layout );
     stable = NULL;
+    show();
 }
 
 VideoWidget::~VideoWidget()
@@ -156,7 +157,6 @@ WId VideoWidget::request( int *pi_x, int *pi_y,
    Parent has to care about resizing itself */
 void VideoWidget::SetSizing( unsigned int w, unsigned int h )
 {
-    if( !isVisible() ) show();
     resize( w, h );
     emit sizeChanged( w, h );
     /* Work-around a bug?misconception? that would happen when vout core resize
@@ -179,7 +179,6 @@ void VideoWidget::release( void )
     stable = NULL;
 
     updateGeometry();
-    hide();
 }
 
 /**********************************************************************
