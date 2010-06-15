b0e55a6bc8b884d1513ad270419883df96a176b2
Ilkka Ollakka <ileoo@videolan.org>
2010-06-15 22:13:06 +0300

    don't use empty string for user-agent (cherry picked from commit e4de986a143b0fdf4ef04b2b5e5286192b3ae4c1)
    Signed-off-by: Ilkka Ollakka <ileoo@videolan.org>

--- src/libvlc-module.c
+++ src/libvlc-module.c
@@ -2055,7 +2055,7 @@ vlc_module_begin ()
                    DATA_PATH_LONGTEXT, true )
         change_need_restart ()
 
-    add_string( "user-agent", "(LibVLC "VERSION")", NULL, "", "", true )
+    add_string( "user-agent", "(LibVLC "VERSION")", NULL, NULL, NULL, true )
         change_safe ()
         change_private ()
 
