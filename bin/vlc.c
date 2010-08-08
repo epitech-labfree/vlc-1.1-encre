8918cf7802951308af497943cdae3f16999f486f
Rémi Denis-Courmont <remi@remlab.net>
2010-08-08 23:07:47 +0300

    Always call _exit() at end to avoid buggy exit handlers
    Removes the KDE scheduler hack.
    (cherry picked from commit 725cf4eb04ab02491301a83d8ce6a5c3f5d80794)
    
    Conflicts:
    
    	bin/vlc.c

--- bin/vlc.c
+++ bin/vlc.c
@@ -200,5 +200,8 @@ int main( int i_argc, const char *ppsz_argv[] )
     for (int i = 1; i < argc; i++)
         LocaleFree (argv[i]);
 
-    return 0;
+    /* Do not run exit handlers. Some of them are buggy (e.g. KDE IO scheduler)
+     * and crash. Also some will crash because their library may be already
+     * unloaded (dlclose()). */
+    _exit (0);
 }
