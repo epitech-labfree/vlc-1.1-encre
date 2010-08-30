a32cc02f8d24910b05984e0b27ced880688d662d
Kaarlo Raiha <kaarlo.raiha@gmail.com>
2010-08-31 00:27:06 +0300

    Associate .flac to raw muxer
    Signed-off-by: Rafaël Carré <rafael.carre@gmail.com>
    (cherry picked from commit c47bb3b7936ba2463b0b17cfacb4302c9b90db4f)
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/stream_out/standard.c
+++ modules/stream_out/standard.c
@@ -250,6 +250,7 @@ static int Open( vlc_object_t *p_this )
             { "ogg", "ogg" },
             { "ogm", "ogg" },
             { "ogv", "ogg" },
+            { "flac","raw" },
             { "mp3", "raw" },
             { "mp4", "mp4" },
             { "mov", "mov" },
